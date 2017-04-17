7d2fb6f524af8eb7ceb9f0a674180b1206c3113a
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-17 11:54:31 -0700

    dried out code for submission

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
 var articleView = {};
 
-articleView.populateFilters = function() {
-  $('article').each(function() {
+articleView.populateFilters = function () {
+  $('article').each(function () {
     if (!$(this).hasClass('template')) {
       var val = $(this).find('address a').text();
       var optionTag = `<option value="${val}">${val}</option>`;
@@ -21,8 +21,8 @@ articleView.populateFilters = function() {
   });
 };
 
-articleView.handleAuthorFilter = function() {
-  $('#author-filter').on('change', function() {
+articleView.handleAuthorFilter = function () {
+  $('#author-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $(`article[data-author="${$(this).val()}"]`).fadeIn();
@@ -30,12 +30,13 @@ articleView.handleAuthorFilter = function() {
       $('article').fadeIn();
       $('article.template').hide();
     }
+
     $('#category-filter').val('');
   });
 };
 
-articleView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
+articleView.handleCategoryFilter = function () {
+  $('#category-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $(`article[data-category="${$(this).val()}"]`).fadeIn();
@@ -43,12 +44,13 @@ articleView.handleCategoryFilter = function() {
       $('article').fadeIn();
       $('article.template').hide();
     }
+
     $('#author-filter').val('');
   });
 };
 
-articleView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
+articleView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function () {
     $('.tab-content').hide();
     $('#' + $(this).data('content')).fadeIn();
   });
@@ -56,52 +58,63 @@ articleView.handleMainNav = function() {
   $('.main-nav .tab:first').click();
 };
 
-articleView.setTeasers = function() {
+articleView.setTeasers = function () {
   $('.article-body *:nth-of-type(n+2)').hide();
 
-  $('#articles').on('click', 'a.read-on', function(e) {
+  $('#articles').on('click', 'a.read-on', function (e) {
     e.preventDefault();
     $(this).parent().find('*').fadeIn();
     $(this).hide();
   });
 };
 
-articleView.initNewArticlePage = function() {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+articleView.initNewArticlePage = function () {
+  $('.tab-content').show();
 
-
-  // TODO: The new articles we create will be copy/pasted into our source data file.
-  // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
-
-  $('#article-json').on('focus', function(){
+  $('#article-json').on('focus', function () {
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the export field with info
-  // from the article that WOULD BE created if any inputs change.
+  $('#article-info').on('change', this.create);
 
 };
 
-articleView.create = function() {
-  // TODO: Set up a var to hold the new article we are creating.
-  // Clear out the #articles element, so we can put in the updated preview
+articleView.create = function () {
+  $('#articles').html('');
 
+  var articleData = {
+    title: $('[name="article-title"]').val(),
+    body: $('[name="article-body"]').val(),
+    author: $('[name="article-author"]').val(),
+    authorUrl: $('[name="author-url"]').val(),
+    category: $('[name="category"]').val(),
+    publishedOn: $('[name="draft"]').attr('checked') ? null : new Date().toString(),
+  };
+  var newArticle = new Article(articleData);
 
-  // TODO: Instantiate an article based on what's in the form fields:
+  var renderFunc = Handlebars.compile($('#preview-template').html());
 
+  // THIS AND THE NEXT BIT OF CODE ARE BOTH SLIGHTLY BROKEN. FIX THEM!
+  // calculate how many days ago this article was published
+  newArticle.daysAgo = parseInt((new Date() - new Date(newArticle.publishedOn)) / 60 / 60 / 24 / 1000);
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+  // set the publication status
+  newArticle.publishStatus = newArticle.publishedOn ? `published ${newArticle.daysAgo} days ago` : '(draft)';
 
+  // render the template with the proper data
+  var renderedHtml = renderFunc(newArticle);
+  $('#articles').append(renderedHtml);
 
-  // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
-  $('pre code').each();
+  $('pre code').each(function (i, block) {
+    hljs.highlightBlock(block);
+  });
 
-  // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-
+  $('#article-json').val(JSON.stringify(newArticle) + ',');
 };
 
+// articleView.initNewArticlePage();
 
-articleView.initIndexPage = function() {
+articleView.initIndexPage = function () {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
